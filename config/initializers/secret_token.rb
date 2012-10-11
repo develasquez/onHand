084fde98bd4ce673fd6a5ce6315535ec72105746
Felipe Velasquez Castro <Felipe.velasquezc@gmail.com>
2012-10-11 00:38:36 -0400

    primero de rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OnHand::Application.config.secret_token = '7128f47fb88cc21bd60674d279a29a0a1f7952408070725d66c444b5c713a6a3d5a10653ab33bd1d5016581211686dddc877ab77da1a1f7ec1df910e05797090'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+OnHand::Application.config.session_store :cookie_store, key: '_onHand_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# OnHand::Application.config.session_store :active_record_store
